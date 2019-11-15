5f25281a92d333d51629ec3dcd64da40b4f547b9
Andrii Oseledko <oseledko.a@dbbest.com>
2019-11-15 17:33:11 +0200

    Optimized solution structure

--- get_expr_type/get_expr_type.sql
+++ get_expr_type/get_expr_type.sql
@@ -9,11 +9,7 @@
 --  Netezza ver.  : 7.2.x
 -- =============================================================================
 --  Description   : Returns the Netezza data type defined for an expression,
---                : previously created as table T_EXPR_TYPE
---                : (schema TEST_NETEZZA_GLUE_TESTER) column.
---                : 
---                : All the appropriate privileges (SELECT, DROP) should be
---                : granted in order for this procedure to work properly.
+--                : previously created as table T_EXPR_TYPE$TABLE column.
 -- =============================================================================
 
 CREATE OR REPLACE PROCEDURE get_expr_type()
@@ -24,15 +20,14 @@ BEGIN_PROC
 DECLARE
     v_expr_type VARCHAR;
 BEGIN
-    SELECT b.format_type
+    SELECT format_type
       INTO v_expr_type
-      FROM _v_table a
-           JOIN _v_relation_column b ON (a.objid = b.objid)
-     WHERE a.tablename = 'T_EXPR_TYPE'
-       AND a.schema = 'TEST_NETEZZA_GLUE_TESTER'
-       AND b.attnum = 1;
+      FROM _v_relation_column
+     WHERE name = 'T_EXPR_TYPE$TABLE'
+       AND owner = CURRENT_USER
+       AND attnum = 1;
 
-    EXECUTE IMMEDIATE 'DROP TABLE t_expr_type IF EXISTS';
+    EXECUTE IMMEDIATE 'DROP TABLE t_expr_type$table IF EXISTS';
 
     RETURN v_expr_type;
 END;
--- get_expr_type/usage_example.sql
+++ get_expr_type/usage_example.sql
@@ -1,5 +1,6 @@
---CTAS from expression
-CREATE TABLE t_expr_type AS
+--CTAS (temp table) from expression
+CREATE TEMP TABLE t_expr_type$table
+AS
 SELECT CAST(56185.15434 AS DEC) + 34 AS expr_val;
 
 --Get the type of expression
